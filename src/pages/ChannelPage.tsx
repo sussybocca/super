032a2ac38ded4472eaa844cd66f763c2991da2a4
sussybocca <babyyodacutefry@gmail.com>
2025-10-19 19:47:18 -0500

    Update ChannelPage.tsx

--- src/pages/ChannelPage.tsx
+++ src/pages/ChannelPage.tsx
@@ -1,26 +1,6 @@
 // src/pages/ChannelPage.tsx
 import { useParams } from "react-router-dom";
-import VideoGrid from "../components/// src/pages/HomePage.tsx
 import VideoGrid from "../components/Video/VideoGrid";
-
-const dummyVideos = Array.from({ length: 12 }).map((_, i) => ({
-  id: `vid-${i}`,
-  title: `Sample Video ${i + 1}`,
-  thumbnail: `https://picsum.photos/300/180?random=${i}`,
-  views: Math.floor(Math.random() * 10000),
-  uploader: `User${i + 1}`
-}));
-
-const HomePage = () => {
-  return (
-    <div className="p-4">
-      <h1 className="text-3xl font-bold mb-4">Trending Videos</h1>
-      <VideoGrid videos={dummyVideos} />
-    </div>
-  );
-};
-
-export default HomePage;ideo/VideoGrid";
 
 const dummyVideos = Array.from({ length: 8 }).map((_, i) => ({
   id: `vid-${i}`,
